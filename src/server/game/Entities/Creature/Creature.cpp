f7f73d4468a570d36a77ca311b20e901fffd2fa7
trickerer <onlysuffering@gmail.com>
2021-01-26 16:36:22 +0700

    Autobalance
    Signed-off-by: newrage <bilndrage66@gmail.com>

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -863,6 +863,8 @@ void Creature::Update(uint32 diff)
         default:
             break;
     }
+
+    sScriptMgr->OnCreatureUpdateAll(this, diff);
 }
 
 void Creature::Regenerate(Powers power)
--- src/server/game/Entities/Creature/Creature.h
+++ src/server/game/Entities/Creature/Creature.h
@@ -27,6 +27,7 @@
 #include "MapObject.h"
 #include <list>
 
+#include "CustomData.h"
 class CreatureAI;
 class CreatureGroup;
 class Group;
@@ -324,6 +325,8 @@ class TC_GAME_API Creature : public Unit, public GridObject<Creature>, public Ma
         void LowerPlayerDamageReq(uint32 unDamage);
         void ResetPlayerDamageReq() { m_PlayerDamageReq = GetHealth() / 2; }
         uint32 m_PlayerDamageReq;
+
+        CreatureCustomData CustomData;
 
         uint32 GetOriginalEntry() const { return m_originalEntry; }
         void SetOriginalEntry(uint32 entry) { m_originalEntry = entry; }
